676f64a7f77914f680d2097a35cc9d472fcecd08
Jeroen Monteban <jeroenmonteban@gmail.com>
2024-01-31 16:06:30 +0100

    update comments

--- setup/aws.tf
+++ setup/aws.tf
@@ -1,14 +1,16 @@
-# AWS OIDC Provider for Terraform Cloud
+# Retrieve TFC certificate
 data "tls_certificate" "tfc_certificate" {
   url = "https://app.terraform.io"
 }
 
+// Create OIDC provider
 resource "aws_iam_openid_connect_provider" "tfc_provider" {
   url             = data.tls_certificate.tfc_certificate.url
   client_id_list  = ["aws.workload.identity"]
   thumbprint_list = [data.tls_certificate.tfc_certificate.certificates[0].sha1_fingerprint]
 }
 
+// Create IAM role for TFC
 resource "aws_iam_role" "tfc_role" {
   name = "tfc-demo-role"
   assume_role_policy = templatefile("./aws_config/tfc_role.tpl",
@@ -21,12 +23,14 @@ resource "aws_iam_role" "tfc_role" {
   })
 }
 
+// Create IAM policy for TFC role
 resource "aws_iam_policy" "tfc_policy" {
   name        = "tfc-demo-policy"
   description = "TFC run policy for demo purposes"
   policy      = file("./aws_config/tfc_policy.json")
 }
 
+// Attach IAM policy to IAM role
 resource "aws_iam_role_policy_attachment" "tfc_policy_attachment" {
   role       = aws_iam_role.tfc_role.name
   policy_arn = aws_iam_policy.tfc_policy.arn
